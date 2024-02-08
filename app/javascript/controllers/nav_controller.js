d666ae70800cbfd6c9a5fa42f4c9979d42c35521
Catalina Galan <catalinagalanvargas@gmail.com>
2024-02-08 16:34:46 -0500

    fixed links in menu and main hero buttons

--- app/javascript/controllers/nav_controller.js
+++ app/javascript/controllers/nav_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="nav"
 export default class extends Controller {
-  static targets = ["navElement", "heroElement", "buttonElement"]
+  static targets = ["navElement", "heroElement", "buttonElement", "menuElement"]
   fire() {
     this.navElementTarget.classList.toggle("hidden");
     this.heroElementTarget.classList.toggle("hidden");
