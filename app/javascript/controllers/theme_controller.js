6c50948c034cfeccd52479ee5346decaf33017ec
Catalina Galan <catalinagalanvargas@gmail.com>
2024-02-16 17:43:39 -0500

    toggle theme almost ready

--- app/javascript/controllers/theme_controller.js
+++ app/javascript/controllers/theme_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="theme"
 export default class extends Controller {
-  static targets = ["themeElement"]
+  static targets = ["themeElement", "toggleElement"]
   // fire() {
   //   this.themeElementTarget.classList.toggle("light");
   // }
@@ -11,14 +11,17 @@ export default class extends Controller {
   }
 
   fire() {
-    const newTheme = this.themeElementTarget.classList.contains("light") ? "dark" : "light";
-    this.themeElementTarget.classList.toggle("light");
-    this.themeElementTarget.classList.toggle("dark");
-    localStorage.setItem('theme', newTheme);
+    console.log("fire");
+    const newTheme = this.toggleElementTarget.checked ? "light" : "dark";
+    this.themeElementTarget.classList.remove("light", "dark");
+    this.themeElementTarget.classList.add(newTheme);
+    localStorage.setItem('toggle', this.toggleElementTarget.checked);
   }
 
   loadTheme() {
-    const theme = localStorage.getItem('theme') || "dark"; // default to dark
+    const toggleState = JSON.parse(localStorage.getItem('toggle')) || false; // default to unchecked (dark theme)
+    this.toggleElementTarget.checked = toggleState;
+    const theme = toggleState ? "light" : "dark";
     this.themeElementTarget.classList.add(theme);
   }
 }
