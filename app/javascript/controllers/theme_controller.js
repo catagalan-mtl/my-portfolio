608fcb5efdd2b9959976eba742ef92d006755c61
Catalina Galan <catalinagalanvargas@gmail.com>
2024-02-16 17:11:27 -0500

    theme stored as user preference

--- app/javascript/controllers/theme_controller.js
+++ app/javascript/controllers/theme_controller.js
@@ -3,7 +3,22 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="theme"
 export default class extends Controller {
   static targets = ["themeElement"]
+  // fire() {
+  //   this.themeElementTarget.classList.toggle("light");
+  // }
+  connect() {
+    this.loadTheme();
+  }
+
   fire() {
+    const newTheme = this.themeElementTarget.classList.contains("light") ? "dark" : "light";
     this.themeElementTarget.classList.toggle("light");
+    this.themeElementTarget.classList.toggle("dark");
+    localStorage.setItem('theme', newTheme);
+  }
+
+  loadTheme() {
+    const theme = localStorage.getItem('theme') || "dark"; // default to dark
+    this.themeElementTarget.classList.add(theme);
   }
 }
